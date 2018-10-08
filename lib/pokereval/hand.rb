b3e2875cd34e4676f01f41c00aac12aef73dcd9a
Laith Shadeed <laith.shadeed@gmail.com>
2018-10-08 14:54:55 +0000

    Make getting the valu of a hand dynamic
     Changes to be committed:
    	modified:   lib/pokereval/hand.rb
    	modified:   spec/pokereval_spec.rb

--- lib/pokereval/hand.rb
+++ lib/pokereval/hand.rb
@@ -2,26 +2,125 @@ module PokerEval
   class Hand
     include Comparable
 
-    attr_reader :value, :weight
+    attr_accessor :hand, :name, :rank, :weight
 
-    VALUES = %i[HIGH_CARD ONE_PAIR TWO_PAIR TREE_OF_A_KIND STRAIGHT FLUSH FULL_HOUSE FOUR_OF_A_KIND STRAIGHT_FLUSH ROYAL_FLUSH]
+    HAND_VALUES = %i[
+      HIGH_CARD
+      ONE_PAIR
+      TWO_PAIR
+      THREE_OF_A_KIND
+      STRAIGHT
+      FLUSH
+      FULL_HOUSE
+      FOUR_OF_A_KIND
+      STRAIGHT_FLUSH
+      ROYAL_FLUSH
+    ].freeze
 
-    def self.from_string(str, val)
-      Hand.new(str.split(' '), val)
+    def self.from_string(str)
+      Hand.new(str)
     end
 
-    def initialize(cards, val)
-      @cards = cards
-      @value = VALUES[val]
-      @weight = val
+    def initialize(str)
+      self.hand = str
+      self.rank = hand
+      self.weight = rank
+      self.name = rank
     end
 
     def <=>(other)
-      weight <=> other.weight
+      @weight <=> other.weight
     end
 
     def to_s
-      "<hand [#{@cards.join(' ')}], '#{@value.to_s.split('_').collect(&:capitalize).join(' ')}'>"
+      "<hand [#{print}], '#{@name}'>"
+    end
+
+    def print
+      @hand.map { |c| "#{c[:rank]}#{c[:suite]}" }
+        .join(' ')
+    end
+
+    def hand=(str)
+      @hand = str
+        .upcase.split(' ')
+        .map(&method(:parse_card))
+        .sort! { |a, b| a[:value] <=> b[:value] }
+    end
+
+    def weight=(rank)
+      @weight = HAND_VALUES.index(rank[0])
+    end
+
+    def name=(rank)
+      @name = rank[0].to_s.split('_').collect(&:capitalize).join(' ')
+    end
+
+    def rank=(hand)
+      (r5, r4, r3, r2, r1) = hand.map { |c| c[:rank] }
+
+      @rank =
+        if straight?
+          r = r1 == :A && r2 == 5 ? 5 : r1
+          if flush?
+            r == :A ? [:ROYAL_FLUSH] : [:STRAIGHT_FLUSH, r]
+          else
+            [:STRAIGHT, r]
+          end
+        elsif r1 == r2 && r2 == r3 && r3 == r4
+          [:FOUR_OF_A_KIND, r1, r5]
+        elsif r1 == r2 && r2 == r3 && r4 == r5
+          [:FULL_HOUSE, r1, r4]
+        elsif r1 == r2 && r3 == r4 && r4 == r5
+          [:FULL_HOUSE, r3, r1]
+        elsif r1 && r2 && r2 != r3 && r3 != r4 && r4 != r5 && flush?
+          [:FLUSH, r1, r2, r3, r4, r5]
+        elsif r1 == r2 && r2 == r3
+          [:THREE_OF_A_KIND, r1, r4, r5]
+        elsif r1 == r2 && r3 == r4
+          [:TWO_PAIR, r1, r3, r5]
+        elsif r1 == r2
+          [:ONE_PAIR, r1, r3, r4, r5]
+        else
+          [:HIGH_CARD, r1, r2, r3, r4, r5]
+        end
+    end
+
+    def straight?
+      (v5, v4, v3, v2, v1) = @hand.map { |c| c[:value] }
+      (v1 == v2 + 1 || (v1 == 14 && v2 == 5)) && # The 14 & 5 to handle A5432 case
+        (v2 == v3 + 1) && (v3 == v4 + 1) && (v4 == v5 + 1)
+    end
+
+    def flush?
+      (s5, s4, s3, s2, s1) = @hand.map { |c| c[:suite] }
+      s1 == s2 && s2 == s3 && s3 == s4 && s4 == s5
+    end
+
+    def parse_card(str)
+      (val, suite) = str.each_char.to_a
+      {rank: parse_rank(val), suite: suite.to_sym, value: parse_value(val)}
+    end
+
+    def parse_rank(str)
+      case str
+      when 'T', 'J', 'Q', 'K', 'A' then
+        str.to_sym
+      else
+        str.to_i
+      end
+    end
+
+    def parse_value(str)
+      case str
+      when 'A' then 14
+      when 'K' then 13
+      when 'Q' then 12
+      when 'J' then 11
+      when 'T' then 10
+      else
+        str.to_i
+      end
     end
   end
 end
--- spec/pokereval_spec.rb
+++ spec/pokereval_spec.rb
@@ -3,38 +3,50 @@ RSpec.describe PokerEval do
     expect(PokerEval::VERSION).not_to be nil
   end
 
-  it 'get hand string' do
-    expect(PokerEval::Hand.from_string('TS JS QS KS AS', 9).to_s)
-      .to eq("<hand [TS JS QS KS AS], 'Royal Flush'>")
+  scenarios = [
+    {name: 'Royal Flush',     str: 'KS JS TS QS AS', out: 'TS JS QS KS AS'},
+    {name: 'Straight Flush',  str: 'AH 5H 2H 4H 3H', out: '2H 3H 4H 5H AH'},
+    {name: 'Straight Flush',  str: '9D KD TD QD JD', out: '9D TD JD QD KD'}
+  ]
+
+  scenarios.each do |s|
+    it "should construct hand from a string of #{s[:str]}" do
+      expect(PokerEval::Hand.from_string(s[:str]).to_s)
+        .to eq("<hand [#{s[:out]}], '#{s[:name]}'>")
+    end
   end
 
   it 'get hand value' do
-    expect(PokerEval::Hand.from_string('TS JS QS KS AS', 9).value)
-      .to eq(:ROYAL_FLUSH)
+    expect(PokerEval::Hand.from_string('TS JS QS KS AS').name)
+      .to eq('Royal Flush')
   end
 
-  it 'assert ROYAL_FLUSH > STRAIGHT_FLUSH' do
-    expect(PokerEval::Hand.from_string('TS JS QS KS AS', 9))
-      .to be > PokerEval::Hand.from_string('5S 6S 7S 8S 9S', 8)
+  it 'should assert ROYAL_FLUSH > STRAIGHT_FLUSH' do
+    expect(PokerEval::Hand.from_string('TS JS QS KS AS'))
+      .to be > PokerEval::Hand.from_string('5S 6S 7S 8S 9S')
   end
 
-  it 'assert FULL_HOUSE < FOUR_OF_A_KIND' do
-    expect(PokerEval::Hand.from_string('5H 5C QD QC QS', 6))
-      .to be < PokerEval::Hand.from_string('7S, TC, TH, TS, TD', 7)
+  it 'should assert FULL_HOUSE < FOUR_OF_A_KIND' do
+    expect(PokerEval::Hand.from_string('5H 5C QD QC QS'))
+      .to be < PokerEval::Hand.from_string('7S TC TH TS TD')
   end
 
-  it 'allow sorting hands' do
+  it 'should sort hands with different rank' do
     hands = [
-      PokerEval::Hand.from_string('5S 6S 7S 8S 9S', 8), # STRAIGHT_FLUSH
-      PokerEval::Hand.from_string('TS JS QS KS AS', 9), # ROYAL_FLUSH
-      PokerEval::Hand.from_string('5H 5C QD QC QS', 6), # FULL_HOUSE
-      PokerEval::Hand.from_string('7S, TC, TH, TS, TD', 7) # FOUR_OF_A_KIND
+      PokerEval::Hand.from_string('2D 3D 7D QD AD'),    # FLUSH
+      PokerEval::Hand.from_string('5S 6S 7S 8S 9S'),    # STRAIGHT_FLUSH
+      PokerEval::Hand.from_string('TS JS QS KS AS'),    # ROYAL_FLUSH
+      PokerEval::Hand.from_string('5H 5C QD QC QS'),    # FULL_HOUSE
+      PokerEval::Hand.from_string('7S TC TH TS TD'),    # FOUR_OF_A_KIND
+      PokerEval::Hand.from_string('4D 5D 6D 7H 8D')     # STRAIGHT
     ]
 
     expect(hands.sort.join(' '))
       .to eq [
+        "<hand [4D 5D 6D 7H 8D], 'Straight'>",
+        "<hand [2D 3D 7D QD AD], 'Flush'>",
         "<hand [5H 5C QD QC QS], 'Full House'>",
-        "<hand [7S, TC, TH, TS, TD], 'Four Of A Kind'>",
+        "<hand [7S TC TH TS TD], 'Four Of A Kind'>",
         "<hand [5S 6S 7S 8S 9S], 'Straight Flush'>",
         "<hand [TS JS QS KS AS], 'Royal Flush'>"
       ].join(' ')
